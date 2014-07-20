2abc7a68a0125ddb712cb65a82b53aca7a7d1747
Alexander Lopatin <sbar.geek@gmail.com>
2014-07-21 01:39:48 +0400

    added empty TagsViewController

---
+++ librefm/librefm/TagsViewController.h
@@ -0,0 +1,13 @@
+//
+//  TagsViewController.h
+//  librefm
+//
+//  Created by sbar on 21/07/14.
+//  Copyright (c) 2014 Alexander Lopatin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TagsViewController : UIViewController
+
+@end
---
+++ librefm/librefm/TagsViewController.m
@@ -0,0 +1,49 @@
+//
+//  TagsViewController.m
+//  librefm
+//
+//  Created by sbar on 21/07/14.
+//  Copyright (c) 2014 Alexander Lopatin. All rights reserved.
+//
+
+#import "TagsViewController.h"
+
+@interface TagsViewController ()
+
+@end
+
+@implementation TagsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
